c94e61e046477e4e732faca913819f9f2da2dcbf
joumanae <joumana.codes@gmail.com>
2020-07-01 17:53:37 -0500

    step 0

--- src/components/VendingMachineAdmin.vue
+++ src/components/VendingMachineAdmin.vue
@@ -252,59 +252,54 @@
 </template>
 
 <script>
-import Vuex from "vuex";
-import Vue from "vue";
-
-Vue.use(Vuex);
 // fake API call //
-let inventory = {
-  chips: {
-    stock: 40
-  }
-};
-var pingInventory = function(item) {
-  return new Promise(resolve => {
-    setTimeout(function() {
-      resolve(inventory[item]);
-    }, 3000);
-  });
-};
+// let inventory = {
+//   chips: {
+//     stock: 40
+//   }
+// };
+// var pingInventory = function(item) {
+//   return new Promise(resolve => {
+//     setTimeout(function() {
+//       resolve(inventory[item]);
+//     }, 3000);
+//   });
+// };
 
-const store = new Vuex.Store({
-  state: {
-    supply: 40,
-    isRestocking: false,
-    isDispensing: false
-  },
-  actions: {
-    fetchFromInventory(context) {
-      context.commit("isRestocking", true);
-      pingInventory("chips")
-        .then(inventory => {
-          context.commit("stockItems", inventory.stock);
-        })
-        .finally(() => context.commit("isRestocking", false));
-    },
-    dispense(context) {
-      context.commit("dispense");
-    }
-  },
-  getters: {},
-  mutations: {
-    isRestocking(state, payload) {
-      state.isRestocking = payload;
-    },
-    dispense(state) {
-      state.supply--;
-    },
-    stockItems(state) {
-      state.supply = 40;
-    }
-  }
-});
+// const store = new Vuex.Store({
+//   state: {
+//     supply: 40,
+//     isRestocking: false,
+//     isDispensing: false
+//   },
+//   actions: {
+//     fetchFromInventory(context) {
+//       context.commit("isRestocking", true);
+//       pingInventory("chips")
+//         .then(inventory => {
+//           context.commit("stockItems", inventory.stock);
+//         })
+//         .finally(() => context.commit("isRestocking", false));
+//     },
+//     dispense(context) {
+//       context.commit("dispense");
+//     }
+//   },
+//   getters: {},
+//   mutations: {
+//     isRestocking(state, payload) {
+//       state.isRestocking = payload;
+//     },
+//     dispense(state) {
+//       state.supply--;
+//     },
+//     stockItems(state) {
+//       state.supply = 40;
+//     }
+//   }
+// });
 export default {
   name: "VendingMachineAdmin",
-  store: store,
   data() {
     return {
       isDispensing: false
--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,48 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+let inventory = {
+  chips: {
+    stock: 40
+  }
+};
+var pingInventory = function(item) {
+  return new Promise(resolve => {
+    setTimeout(function() {
+      resolve(inventory[item]);
+    }, 3000);
+  });
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    supply: 40,
+    isRestocking: false,
+    isDispensing: false
+  },
+  actions: {
+    fetchFromInventory(context) {
+      context.commit("isRestocking", true);
+      pingInventory("chips")
+        .then(inventory => {
+          context.commit("stockItems", inventory.stock);
+        })
+        .finally(() => context.commit("isRestocking", false));
+    },
+    dispense(context) {
+      context.commit("dispense");
+    }
+  },
+  getters: {},
+  mutations: {
+    isRestocking(state, payload) {
+      state.isRestocking = payload;
+    },
+    dispense(state) {
+      state.supply--;
+    },
+    stockItems(state) {
+      state.supply = 40;
+    }
+  }
 });
